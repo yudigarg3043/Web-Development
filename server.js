e1fca1d71d8eb7a5d5b1f4057b5b7afac7572461
yudigarg3043 <yudishgarg645@gmail.com>
2025-07-30 11:23:13 +0530

    Renaming Files

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var app = express();
 
-var port = 8080;
-app.use(express.static('public'));
+var port = 80;
+app.use(express.static('frontend'));
 
 app.listen(port, function(){
     console.log(`Server is running on http://localhost:${port}`);
